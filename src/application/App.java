e5a550c6321974e3861f67cddb5a6e4850f969c5
Kai_To <aelatrouz@gmail.com>
2019-12-02 21:28:23 +0100

    adding the interface's things

--- src/application/App.java
+++ src/application/App.java
@@ -3,20 +3,20 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
-public class App extends Application {
+public class App extends Application { 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			AnchorPane root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("Style/application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch(Exception e) { 
 			e.printStackTrace();
 		}
 	}
--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,5 +1,44 @@
 package application;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SampleController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+public class SampleController implements Initializable{ 
+	
+    @FXML
+	
+	public void changeScene(ActionEvent event) throws IOException {
+		AnchorPane pane=FXMLLoader.load(getClass().getResource("home.fxml"));
+		Scene scene=new Scene(pane);
+		Stage stage=(Stage) ((Node) event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
+	}
+    
+    @FXML
+	private MediaView mediaView;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {	
+		Media media=new Media("file:///C:/Users/asus/Desktop/HMS/hospital-management-systeme/src/application/video/backVedio.mp4");
+		MediaPlayer player=new MediaPlayer(media);
+		player.setAutoPlay(true);
+		mediaView.setMediaPlayer(player);
+		player.setVolume(0);
+		player.play();
+	
+	}
 	
 }
