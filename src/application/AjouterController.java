77e375e00177e94b57160d46e97634ea11eaa78a
Youssef DAFALI <dafaliyoussef01@gmail.com>
2019-12-04 00:03:21 +0100

    new update

---
+++ src/application/AjouterController.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class AjouterController implements Initializable{
+	
+	
+	@FXML
+	
+	public void goBackToPatient(ActionEvent event) throws IOException {
+	  	AnchorPane pane=FXMLLoader.load(getClass().getResource("Patient.fxml"));
+	  	Scene scene=new Scene(pane);
+	  	Stage stage=(Stage) ((Node) event.getSource()).getScene().getWindow();
+	  	stage.setScene(scene);
+	  	stage.show();
+	  	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		
+	}
+
+}
